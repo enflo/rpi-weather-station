4153cba6e2a60ade18315307ba0b6a342dd7a767
Toni <toniflorithomar@gmail.com>
2023-03-02 18:32:09 +0100

    add temperature humidity sensor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from src.air_quality import AirQualityMonitor
+from src.dht22 import TemperatureHumidityMonitor
+from src.sds011 import AirQualityMonitor
 
 
 if __name__ == "__main__":
     air_quality = AirQualityMonitor().get_measurement()
+    temperature_humidity = TemperatureHumidityMonitor().get_measurement()
     print(air_quality)
+    print(temperature_humidity)
---
+++ src/dht22.py
@@ -0,0 +1,29 @@
+import adafruit_dht
+import board
+
+
+class TemperatureHumidityMonitor:
+
+    def __init__(self):
+        self.dht = adafruit_dht.DHT22(board.D18)
+
+    def get_measurement(self):
+        temperature_f, temperature_c, humidity = self.get_temperature_humidity()
+        return {
+            "temperature_farenheit": temperature_f,
+            "temperature_celsius": temperature_c,
+            "humidity": humidity,
+        }
+
+    def get_temperature_humidity(self):
+        try:
+            temperature_c = self.dht.temperature
+            temperature_f = temperature_c * (9 / 5) + 32
+            humidity = self.dht.humidity
+            return temperature_f, temperature_c, humidity
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, just keep going
+            raise error
+        except Exception as error:
+            self.dht.exit()
+            raise error
