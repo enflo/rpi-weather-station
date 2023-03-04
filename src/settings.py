14c3e7ecac1a65c88d62944be34fe7af0c9e74f2
Toni <toniflorithomar@gmail.com>
2023-03-04 18:13:01 +0100

    Change import

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,10 @@
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path('.', '.env')
+load_dotenv(dotenv_path=env_path)
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
