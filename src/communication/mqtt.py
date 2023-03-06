531b12744653e4a772f7be464df1072e79e9c9e7
Toni <toniflorithomar@gmail.com>
2023-03-06 20:17:08 +0100

    Use envm,

--- src/communication/mqtt.py
+++ src/communication/mqtt.py
@@ -19,7 +19,7 @@ class SendDataMQTT:
         client = mqtt.Client(transport="websockets")
         client.tls_set()
         client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-        client.connect(MQTT_HOST, MQTT_PORT, 30)
+        client.connect(MQTT_HOST, int(MQTT_PORT), 30)
         return client
 
     @staticmethod
